d19dee9fa423c8d5d2c5d1608b8c2b819fed5833
darkvuk <dark.vukasovic@gmail.com>
2024-01-28 14:16:25 +0100

    Add Jobs And Blogs

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='image')
 
     def __str__(self):
         return self.title
